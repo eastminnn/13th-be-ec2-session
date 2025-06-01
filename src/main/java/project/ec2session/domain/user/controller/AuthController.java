ce3d70b04ca2c2b663ab927ec54f897ee30b882b
eastminnn <oh1334000@gmail.com>
2025-06-01 15:27:06 +0900

    docs : sign-up swagger 작성

--- src/main/java/project/ec2session/domain/user/controller/AuthController.java
+++ src/main/java/project/ec2session/domain/user/controller/AuthController.java
@@ -1,6 +1,13 @@
 package project.ec2session.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jdk.jfr.ContentType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +24,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "인증 관련 API", description = "인증에 관련한 API (회원가입, 로그인)")
 public class AuthController{
     private final AuthService authService;
 
     @PostMapping("/sign-in")
+    @Operation(summary = "로그인", description = "로그인 시도")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                    "accessToken" : "<accessToken>",
+                                    "refreshToken" : "<refreshToken>"
+                                    }
+                                    """)
+                    })),
+            @ApiResponse(responseCode = "500", description = "서버 에러")
+    })
     public ResponseEntity<?> signIn(
             @RequestBody @Valid UserReq.SignInDto request
     ) {
@@ -29,6 +50,36 @@ public class AuthController{
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(summary = "회원가입", description = "회원가입 시도")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                    "userId" : 1
+                                    }
+                                    """)
+                    })),
+            @ApiResponse(responseCode = "500", description = "서버 에러",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                    "status" : 500,
+                                    "message" : "서버 에러입니다. 서버 팀에 연락주세요."
+                                    }
+                                    """)
+                    })),
+            @ApiResponse(responseCode = "400", description = "필수 입력 값 누락",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                    "password": "비밀번호는 필수 입력 값입니다.",
+                                    "nickname": "닉네임은 필수 입력 값입니다.",
+                                    "username": "아이디는 필수 입력 값입니다."
+                                    }
+                                    """)
+                    }))
+    })
     @PostMapping("/sign-up")
     public ResponseEntity<?> signUp(@RequestBody @Valid UserReq.SignUpDto request) {
         Long userId = authService.signUp(request);
